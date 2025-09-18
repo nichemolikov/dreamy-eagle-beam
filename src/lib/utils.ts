08764768b13e1eaba6a9d09775d11d0e4a5fb04c
[dyad] <nichepulsemedia@gmail.com>
2025-09-18 19:28:06 +0300

    [dyad] Added currency formatting utility - wrote 1 file(s)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function formatCurrency(amount: number, currency: string = "BGN", locale: string = "bg-BG") {
+  return new Intl.NumberFormat(locale, {
+    style: "currency",
+    currency: currency,
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(amount);
+}
