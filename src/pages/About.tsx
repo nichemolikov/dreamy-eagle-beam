ab5a951a27cd200c6f2319cffbef9c19c7ff58a7
[dyad] <nichepulsemedia@gmail.com>
2025-09-18 07:49:44 +0300

    [dyad] Актуализиране на банер изображението на страницата "За нас" - wrote 1 file(s)

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,7 +7,7 @@ const About = () => {
     <div>
       <section
         className="relative w-full h-[30vh] bg-cover bg-center"
-        style={{ backgroundImage: "url(https://via.placeholder.com/1920x400?text=About+Us+Banner)" }}
+        style={{ backgroundImage: "url(https://wallpaperaccess.com/full/1669455.jpg)" }}
       >
         <div className="absolute inset-0 bg-gradient-to-b from-black/50 to-transparent" />
         <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white p-4">
